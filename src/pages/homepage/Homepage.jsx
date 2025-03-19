2173468529dfd8cad7f745adead054493df20a30
Oluwasanmij2r4 <137338488+Oluwasanmij2r4@users.noreply.github.com>
2025-03-19 01:27:12 +0100

    Add styling for the music card

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -5,7 +5,7 @@ const Homepage = () => {
   return (
     <body>
       <header>
-        <div className={style.headerContainer} >
+        <div className={style.headerContainer}>
           <nav>
             <ul>
               <li>
@@ -25,7 +25,7 @@ const Homepage = () => {
           </nav>
 
           <article>
-            <div className={style.articleContainer} >
+            <div className={style.articleContainer}>
               <h1>Own the Sound.</h1>
               <h1>Fuel the Future.</h1>
 
@@ -35,7 +35,9 @@ const Homepage = () => {
                 trade—because music should be yours to own.
               </p>
               <div className={style.button}>
-                <a href="#" className={style.rightButton} >Get Started</a>
+                <a href="#" className={style.rightButton}>
+                  Get Started
+                </a>
                 <a href="#">Explore</a>
               </div>
             </div>
@@ -43,8 +45,51 @@ const Homepage = () => {
         </div>
       </header>
 
-      <main>
-        
+      <main className={style.mainBody}>
+        <h3>Trending Now</h3>
+
+        <article className={style.musicCard}>
+          <div>
+            <img src="/asset/tamara.jpg" alt="album cover" />
+
+            <h4>P.Jay</h4>
+            <span>
+              <a href="#">Buy</a>
+
+              <p>20 Sui</p>
+            </span>
+          </div>
+
+          <div>
+            <img src="/asset/howe.jpg" alt="album cover" />
+
+            <h4>CharBae</h4>
+            <span>
+              <a href="#">Buy</a>
+              <p>15 Sui</p>
+            </span>
+          </div>
+
+          <div>
+            <img src="/asset/dreokt.jpg" alt="album cover" />
+
+            <h4>R.Power</h4>
+            <span>
+              <a href="#">Buy</a>
+              <p>5 Sui</p>
+            </span>
+          </div>
+
+          <div>
+            <img src="/asset/marcela.jpg" alt="album cover" />
+
+            <h4>Oracle</h4>
+            <span>
+              <a href="#">Buy</a>
+              <p>6 Sui</p>
+            </span>
+          </div>
+        </article>
       </main>
     </body>
   );
