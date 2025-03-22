d4aba2c4ede7a1a79fcfd50e71799df533d5d33c
Oluwasanmij2r4 <137338488+Oluwasanmij2r4@users.noreply.github.com>
2025-03-22 10:46:57 +0100

    Change the header layout

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -14,112 +14,105 @@ import MarketPlace from "../MarketPlace/MarketPlace";
 
 const Homepage = () => {
   return (
-    <div>
-      <header>
-        <div className={style.headerContainer}>
-          <nav>
-            <div className={style.logoContainer} >
-              <Link to="/">
-                <Soundicon className={style.headerIcon} />
-
-                <h1 className={style.logoText}>WaveKey</h1>
-              </Link>
-            </div>
-
-            <ul className={style.navLink}>
-              <li>
-                <a href="#">Sign In</a>
-              </li>
-              <li>
-                <a href="#">Create Account</a>
-              </li>
-            </ul>
-          </nav>
-
-          <article>
-            <div className={style.articleContainer}>
-              <h1>Own the Sound. <br/>Fuel the Future.</h1>
-
-              <p>
-                Discover exclusive music NFTs, support your favorite artists,
-                and unlock premium experiences. Stream, collect, and
-                trade—because music should be yours to own.
-              </p>
-              <div className={style.button}>
-                <a href="#">
-                  <button className={style.rightButton}>Get Started</button>
-                </a>
-                <a href="#">
-                  <button className={style.leftButton}>Explore</button>
-                </a>
+    <div className={style.body} >
+      <div className={style.container}>
+        <header>
+          <div className={style.headerContainer}>
+            <nav>
+              <div className={style.logoContainer} >
+                <Link to="/">
+                  <Soundicon className={style.headerIcon} />
+                  <h1 className={style.logoText}>SuiTunes</h1>
+                </Link>
               </div>
-            </div>
-          </article>
-        </div>
-      </header>
-
-      <main className={style.mainBody}>
-        <section>
-          <h3>Trending Now</h3>
-          <article className={style.musicCard}>
-            <div>
-              <img src="/assets/zachary.jpg" alt="album cover" />
-              <h4>P.Jay</h4>
-              <span>
-                <a href="#">Buy</a>
-                <p>20 Sui</p>
-              </span>
-            </div>
-            <div>
-              <img src="/assets/howe.jpg" alt="album cover" />
-              <h4>CharBae</h4>
-              <span>
-                <a href="#">Buy</a>
-                <p>15 Sui</p>
-              </span>
-            </div>
-            <div>
-              <img src="/assets/dreokt.jpg" alt="album cover" />
-              <h4>R.Power</h4>
-              <span>
-                <a href="#">Buy</a>
-                <p>5 Sui</p>
-              </span>
-            </div>
-            <div>
-              <img src="/assets/marcela.jpg" alt="album cover" />
-              <h4>Oracle</h4>
-              <span>
-                <a href="#">Buy</a>
-                <p>6 Sui</p>
-              </span>
-            </div>
-          </article>
-        </section>
-
-        {/* <section>
-          <h3>How it Works</h3>
-          <article className={style.stepsContainer}>
-            <div className={style.stepsCard}>
-              <h2>1. Upload Music</h2>
-              <div className={style.iconCard}>
-                <Uploadicon className={style.stepsIcon} />
+              <ul className={style.navLink}>
+                <li>
+                  <a href="#"><button>Get Started</button></a>
+                </li>
+              </ul>
+            </nav>
+            <article>
+              <div className={style.articleContainer}>
+                <h1>Own the Sound. <br/>Fuel the Future.</h1>
+                <p>
+                  Discover exclusive music NFTs, support your favorite artists,
+                  and unlock premium experiences. Stream, collect, and
+                  trade—because music should be yours to own.
+                </p>
+                <div className={style.button}>
+                  <a href="#">
+                    <button className={style.rightButton}>Get Started</button>
+                  </a>
+                  <a href="#">
+                    <button className={style.leftButton}>Explore</button>
+                  </a>
+                </div>
               </div>
-            </div>
-            <div>
-              <h4>Mint as NFTs</h4>
-            </div>
-            <div>
-              <h4>Sell to your Fave</h4>
-            </div>
-            <div>
-              <h4>Get Paid Instantly</h4>
-              
-            </div>
-          </article>
-          <MarketPlace/>
-        </section> */}
-      </main>
+            </article>
+          </div>
+        </header>
+        <main className={style.mainBody}>
+          <section>
+            <h3>Trending Now</h3>
+            <article className={style.musicCard}>
+              <div>
+                <img src="/assets/zachary.jpg" alt="album cover" />
+                <h4>P.Jay</h4>
+                <span>
+                  <a href="#">Buy</a>
+                  <p>20 Sui</p>
+                </span>
+              </div>
+              <div>
+                <img src="/assets/howe.jpg" alt="album cover" />
+                <h4>CharBae</h4>
+                <span>
+                  <a href="#">Buy</a>
+                  <p>15 Sui</p>
+                </span>
+              </div>
+              <div>
+                <img src="/assets/dreokt.jpg" alt="album cover" />
+                <h4>R.Power</h4>
+                <span>
+                  <a href="#">Buy</a>
+                  <p>5 Sui</p>
+                </span>
+              </div>
+              <div>
+                <img src="/assets/marcela.jpg" alt="album cover" />
+                <h4>Oracle</h4>
+                <span>
+                  <a href="#">Buy</a>
+                  <p>6 Sui</p>
+                </span>
+              </div>
+            </article>
+          </section>
+          {/* <section>
+            <h3>How it Works</h3>
+            <article className={style.stepsContainer}>
+              <div className={style.stepsCard}>
+                <h2>1. Upload Music</h2>
+                <div className={style.iconCard}>
+                  <Uploadicon className={style.stepsIcon} />
+                </div>
+              </div>
+              <div>
+                <h4>Mint as NFTs</h4>
+              </div>
+              <div>
+                <h4>Sell to your Fave</h4>
+              </div>
+              <div>
+                <h4>Get Paid Instantly</h4>
+        
+              </div>
+            </article>
+            <MarketPlace/>
+          </section> */}
+        </main>
+      </div>
     </div>
   );
 };
