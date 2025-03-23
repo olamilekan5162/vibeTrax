15a7009f81b05dc70b4a80d4bef031fe61a40b66
Oluwasanmij2r4 <137338488+Oluwasanmij2r4@users.noreply.github.com>
2025-03-23 08:04:48 +0100

    Add animation to music card

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -14,47 +14,52 @@ import MarketPlace from "../MarketPlace/MarketPlace";
 
 const Homepage = () => {
   return (
-    <div className={style.body} >
-      <div className={style.container}>
-        <header>
-          <div className={style.headerContainer}>
-            <nav>
-              <div className={style.logoContainer} >
-                <Link to="/">
-                  <Soundicon className={style.headerIcon} />
-                  <h1 className={style.logoText}>SuiTunes</h1>
-                </Link>
+    <div className={style.body}>
+      <header>
+        <div className={style.headerContainer}>
+          <nav>
+            <div className={style.logoContainer}>
+              <Link to="/">
+                <Soundicon className={style.headerIcon} />
+                <h1 className={style.logoText}>SuiTunes</h1>
+              </Link>
+            </div>
+            <ul className={style.navLink}>
+              <li>
+                <a href="#">
+                  <button>Get Started</button>
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <article>
+            <div className={style.articleContainer}>
+              <h1>
+                Own the Sound. <br />
+                Fuel the Future.
+              </h1>
+              <p>
+                Discover exclusive music NFTs, support your favorite artists,
+                and unlock premium experiences. Stream, collect, and
+                trade—because music should be yours to own.
+              </p>
+              <div className={style.button}>
+                <a href="#">
+                  <button className={style.rightButton}>Get Started</button>
+                </a>
+                <a href="#">
+                  <button className={style.leftButton}>Explore</button>
+                </a>
               </div>
-              <ul className={style.navLink}>
-                <li>
-                  <a href="#"><button>Get Started</button></a>
-                </li>
-              </ul>
-            </nav>
-            <article>
-              <div className={style.articleContainer}>
-                <h1>Own the Sound. <br/>Fuel the Future.</h1>
-                <p>
-                  Discover exclusive music NFTs, support your favorite artists,
-                  and unlock premium experiences. Stream, collect, and
-                  trade—because music should be yours to own.
-                </p>
-                <div className={style.button}>
-                  <a href="#">
-                    <button className={style.rightButton}>Get Started</button>
-                  </a>
-                  <a href="#">
-                    <button className={style.leftButton}>Explore</button>
-                  </a>
-                </div>
-              </div>
-            </article>
-          </div>
-        </header>
-        <main className={style.mainBody}>
-          <section>
-            <h3>Trending Now</h3>
-            <article className={style.musicCard}>
+            </div>
+          </article>
+        </div>
+      </header>
+      <main className={style.mainBody}>
+        <section>
+          <h2>Trending Now</h2>
+          <article className={style.musicGallery}>
+            <div className={style.musicAnimate}>
               <div>
                 <img src="/assets/zachary.jpg" alt="album cover" />
                 <h4>P.Jay</h4>
@@ -87,9 +92,45 @@ const Homepage = () => {
                   <p>6 Sui</p>
                 </span>
               </div>
-            </article>
-          </section>
-          {/* <section>
+
+              {/* Duplicate */}
+
+              <div>
+                <img src="/assets/zachary.jpg" alt="album cover" />
+                <h4>P.Jay</h4>
+                <span>
+                  <a href="#">Buy</a>
+                  <p>20 Sui</p>
+                </span>
+              </div>
+              <div>
+                <img src="/assets/howe.jpg" alt="album cover" />
+                <h4>CharBae</h4>
+                <span>
+                  <a href="#">Buy</a>
+                  <p>15 Sui</p>
+                </span>
+              </div>
+              <div>
+                <img src="/assets/dreokt.jpg" alt="album cover" />
+                <h4>R.Power</h4>
+                <span>
+                  <a href="#">Buy</a>
+                  <p>5 Sui</p>
+                </span>
+              </div>
+              <div>
+                <img src="/assets/marcela.jpg" alt="album cover" />
+                <h4>Oracle</h4>
+                <span>
+                  <a href="#">Buy</a>
+                  <p>6 Sui</p>
+                </span>
+              </div>
+            </div>
+          </article>
+        </section>
+        {/* <section>
             <h3>How it Works</h3>
             <article className={style.stepsContainer}>
               <div className={style.stepsCard}>
@@ -111,8 +152,7 @@ const Homepage = () => {
             </article>
             <MarketPlace/>
           </section> */}
-        </main>
-      </div>
+      </main>
     </div>
   );
 };
