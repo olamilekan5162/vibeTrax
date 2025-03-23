15576a3e838d0ef1ef88f8f21a0f9f1a1fb9ae4c
olamilekan5162 <opeyemiolalekan481@gmail.com>
2025-03-23 13:04:24 +0100

    Work on the the sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,20 +1,37 @@
 import styles from './sidebar.module.css';
-import {NavLink} from 'react-router-dom'
+import { NavLink, Link} from 'react-router-dom';
+import { SiBluesound } from "react-icons/si";
 
 const Sidebar = () => {
   return(
     <div className={styles.stickySidebar}>
+      
+      <div className={styles.logoContainer}>
+        <Link to="/">
+          <SiBluesound className={styles.headerIcon} />
+          <h1 className={styles.logoText}>SuiTunes</h1>
+        </Link>
+      </div>
+      
       <div className={styles.barContainer}>
-        <NavLink to='dashboard'>
-          Dashboard
+        <NavLink className={styles.inactive} to='dashboard'>
+          Home
         </NavLink>
         
-        <NavLink to='dashboard'>
-          Dashboard
+        <NavLink className={styles.active}to='Marketplace'>
+          Marketplace
         </NavLink>
         
-        <NavLink to='dashboard'>
-          Dashboard
+        <NavLink className={styles.inactive} to='nft'>
+          My NFTs
+        </NavLink>
+        
+        <NavLink className={styles.inactive} to='profile'>
+          Profile
+        </NavLink>
+      
+        <NavLink className={styles.inactive} to='settings'>
+          Settings
         </NavLink>
         
       </div>
