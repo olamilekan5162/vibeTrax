dcb46073072c3eb430ba7ac9f7857eecd1d16a44
olamilekan5162 <opeyemiolalekan481@gmail.com>
2025-03-25 15:12:23 +0100

    Add profile image to sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,7 @@
 import styles from './sidebar.module.css';
 import { NavLink, Link} from 'react-router-dom';
 import { SiBluesound } from "react-icons/si";
+import profilePic from "/assets/austin.jpg"
 
 const Sidebar = () => {
   return(
@@ -13,6 +14,10 @@ const Sidebar = () => {
         </Link>
       </div>
       
+      <div className={styles.imgContainer} >
+            <img src={profilePic} alt="profilePic" />
+        </div>
+        
       <div className={styles.barContainer}>
         <NavLink className={({isActive}) => (isActive ? styles.active : styles.inactive)} to='/homepage' end>
           Home
