6f38c42f475b85d338e93d812edd16f2e92cfbe7
Oluwasanmij2r4 <137338488+Oluwasanmij2r4@users.noreply.github.com>
2025-04-15 09:38:43 +0100

    Add , to line 31 or router.jsx bcause it shows error there

--- src/router.jsx
+++ src/router.jsx
@@ -24,7 +24,7 @@ export const router = createBrowserRouter([
       },{
         path: "music-player",
         Component: MusicPlayer,
-      }
+      },
       {
         path: "upload",
         Component: UploadMusic,
