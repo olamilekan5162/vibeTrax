62a2a6a439ac4332421c051af04e1375f9915038
Freedteck <freedteck@gmail.com>
2025-04-15 01:06:35 +0100

    Add PremiumModal component and styles for premium upgrade feature

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import Banner from "../../components/banner/Banner";
 import CtaComponent from "../../components/cta-section/CtaComponent";
 import SectionContainer from "../../components/section-container/SectionContainer";
+import PremiumModal from "../../modals/premium-modal/PremiumData";
 import {
   featuresData,
   stepsData,
@@ -8,6 +10,7 @@ import {
 } from "../../samples/homeSample";
 import styles from "./Home.module.css";
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <main className={styles.home}>
       <Banner />
@@ -33,6 +36,8 @@ const Home = () => {
         subtitle="Join TuneFlow today and be part of the revolution that's creating a fairer, more transparent music ecosystem for artists and fans alike."
         buttonText="Get Started Today"
       />
+      <button onClick={() => setIsOpen(true)}>open</button>
+      <PremiumModal isOpen={isOpen} onClose={() => setIsOpen(false)} />
     </main>
   );
 };
